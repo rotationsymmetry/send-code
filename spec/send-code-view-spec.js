88f43e092a59a6cb8373324c7076554f76ec7254
Meihua Wu <meihua_wu@apple.com>
2017-10-07 09:58:52 -0700

    shortcut, menu, clean up.

--- spec/send-code-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SendCodeView from '../lib/send-code-view';
-
-describe('SendCodeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
