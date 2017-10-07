b44f683eb7c6b367705b3a577aab05db1f2be8aa
Meihua Wu <meihua_wu@apple.com>
2017-10-07 09:46:09 -0700

    working example.

---
+++ spec/send-code-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SendCodeView from '../lib/send-code-view';
+
+describe('SendCodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
